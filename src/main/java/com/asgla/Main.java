5c054a6e58f579ffc221d57cfca1af97d598c967
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2021-10-10 15:47:51 -0300

    Updated pom and database test

--- src/main/java/com/asgla/Main.java
+++ src/main/java/com/asgla/Main.java
@@ -1,9 +1,19 @@
 package com.asgla;
 
+import com.asgla.db.Database;
+import com.asgla.db.model.user.User;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
-    public static void main(String[] params) {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(Main.class);
 
+    public static void main(String[] params) {
+        Database.open();
+
+        log.info("Test {}", User.findById(9).getId());
+
+        Database.close();
     }
 
 }
