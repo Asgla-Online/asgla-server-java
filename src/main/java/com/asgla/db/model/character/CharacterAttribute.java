d4003aa6b982b91303f2c8b46f17f80dbe2a5c76
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2021-10-16 23:01:37 -0300

    Character attribute

---
+++ src/main/java/com/asgla/db/model/character/CharacterAttribute.java
@@ -0,0 +1,79 @@
+package com.asgla.db.model.character;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Cached;
+import org.javalite.activejdbc.annotations.Table;
+
+@Cached
+@Table("characters_attributes")
+public class CharacterAttribute extends Model {
+
+    public Integer characterId() {
+        return getInteger("character_id");
+    }
+
+    public String gold() {
+        return getString("gold");
+    }
+
+    public String diamond() {
+        return getString("diamond");
+    }
+
+    public String gender() {
+        return getString("gender");
+    }
+
+    public String access() {
+        return getString("access");
+    }
+
+    public String level() {
+        return getString("level");
+    }
+
+    public String experience() {
+        return getString("experience");
+    }
+
+    public String colorAccessory() {
+        return getString("color_accessory");
+    }
+
+    public String colorBase() {
+        return getString("color_base");
+    }
+
+    public String colorTrim() {
+        return getString("color_trim");
+    }
+
+    public String colorSkin() {
+        return getString("color_skin");
+    }
+
+    public String colorEye() {
+        return getString("color_eye");
+    }
+
+    public String colorHair() {
+        return getString("color_hair");
+    }
+
+    public String colorMouth() {
+        return getString("color_mouth");
+    }
+
+    public String colorNose() {
+        return getString("color_nose");
+    }
+
+    public String bagSlot() {
+        return getString("bag_Slot");
+    }
+
+    public Character character() {
+        return Character.findById(characterId());
+    }
+
+}
