f11deba875765c30f131de5159beb1b1df7cc7c5
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2021-11-03 07:18:26 -0300

    Inventory load request

--- src/main/java/com/asgla/requests/unity/player/InventoryLoad.java
+++ src/main/java/com/asgla/requests/unity/player/InventoryLoad.java
@@ -4,12 +4,21 @@ import com.asgla.avatar.player.Player;
 import com.asgla.db.annotations.DatabaseOpen;
 import com.asgla.requests.IRequest;
 import com.asgla.requests.RequestArgs;
+import com.asgla.util.RequestCommand;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 @DatabaseOpen
 public class InventoryLoad implements IRequest {
 
     @Override
     public void onRequest(Player player, RequestArgs args) {
+        JSONObject pli = new JSONObject()
+            .element("cmd", RequestCommand.PlayerInventoryLoad)
+            .element("playerId", player.user().id())
+            .element("inventory", new JSONArray());
+
+        player.dispatch(pli);
     }
 
 }
